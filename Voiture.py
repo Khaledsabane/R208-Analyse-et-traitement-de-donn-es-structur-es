31fb4cfb28564df1ff69aa7730a2cb5396d0b415
Khaledsabane <148535782+Khaledsabane@users.noreply.github.com>
2024-02-22 11:36:24 +0100

    Add files via upload

---
+++ Voiture.py
@@ -0,0 +1,37 @@
+class Voiture:
+    def __init__(self,marque='X',modele='s-line',puissance_Fiscale='3',couleur='Noir'):
+        self.__marque=marque
+        self.__modele=modele
+        self.__puissance_Fiscale=puissance_Fiscale
+        self.__couleur=couleur
+        self.__option = []
+    def getmarque(self):
+        return self.__marque
+    def getmodele(self):
+        return self.__modele
+    def getpuissance(self):
+        return self.__puissance_Fiscale
+    def getcouleur(self):
+        return self.__couleur
+    def getoption(self):
+        return self.__option
+
+
+    def setmarque(self,m):
+        self.__marque=m
+    def setmodele(self,m):
+        self.__modele=m
+    def setpuissance(self,m):
+        self.__puissance_Fiscale=m
+    def setcouleur(self,m):
+        self.__couleur=m
+    def setoption(self,m):
+        self.__option=m
+
+
+    def ajouter_option(self, opts) :
+        self.__option.append(opts)
+
+    def __str__(self):
+        return f"Voici les caractéristiques de cette voiture: \n -Marque : {self.__marque} \n- Modele : {self.__modele} \n - Couleur : {self.__couleur} \n - puissance : {self.__puissance_Fiscale} \n- options : {self.__option}"
+
---
+++ main.py
@@ -0,0 +1,19 @@
+from Voiture import Voiture
+if __name__=="__main__":
+    myCar1=Voiture("Opel","Clio","2")
+    myCar2=Voiture()
+    myCar3=Voiture("Model S", "Tesla","5")
+    myCar4=Voiture(couleur='Rouge')
+    print(f"Voiture {myCar1.getmarque()}")
+
+    myCar1.setmarque('BMW')
+    myCar2.setmarque('COROLLA')
+    myCar1.__str__()
+    myCar2.__str__()
+    print(f"Voiture {myCar1}")
+    print(f"Voiture {myCar2}")
+    myCar1.ajouter_option('autoparking')
+    myCar1.ajouter_option('autodriving')
+    print(f"Voiture {myCar1}")
+
+
