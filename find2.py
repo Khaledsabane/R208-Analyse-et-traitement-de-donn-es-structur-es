5123fd73adc03c2743c66eda17b505ca1855522e
Khaledsabane <148535782+Khaledsabane@users.noreply.github.com>
2024-03-07 14:33:12 +0100

    Update find2.py

--- find2.py
+++ find2.py
@@ -1,24 +1,45 @@
-import sys
-import os
-
-
+import os,sys
+lf=[]
+ld=[]
 def recherche(dossier):
     liste = os.listdir(dossier)
-    listeFichiers = []
-    listeDossiers = []
 
-    print(liste)
-    for elt in liste:
-        chemin = os.path.join(dossier, elt)
-        if os.path.isfile(chemin):
-            listeFichiers.append(dossier + elt)
-            # elt correspond au dossier à ajouter dans la liste
-        # ‘dossier + elt’ réprésente le chemin complet sur le fichier
-        elif os.path.isdir(chemin):
-            listeDossiers.append(dossier + elt)
-    print(f"les fichier: {listeFichiers}")
-    print(f"les dossiers: {listeDossiers}")
-    
-if __name__== '__main__':
-    dossier=sys.argv[1]
-    recherche(dossier)
+    listeFichiers=[]
+    listeDossiers=[]
+
+    for elt in liste :
+
+        if  os.path.isfile(f"{dossier}\{elt}") :
+             listeFichiers.append(f"{dossier}\{elt}")
+
+
+             # elt correspond au dossier à ajouter dans la
+
+             # ‘dossier + elt’ réprésente le chemin complet sur le fichier
+        elif os.path.isdir(f"{dossier}\{elt}"):
+             listeDossiers.append(f"{dossier}\{elt}")
+    return listeFichiers, listeDossiers
+
+def affiche(l):
+    for e in l:
+        print(e)
+    return
+
+def start_find2():
+    lf,ld =recherche(sys.argv[1])
+    for i in ld:
+        lf2,ld2=recherche(i)
+        lf.extend(lf2)
+        ld.extend(ld2)
+    print(f"la liste des dossiers :")
+    affiche(ld)
+    print(f"la liste des fichiers :")
+    affiche(lf)
+
+
+
+
+
+if __name__=='__main__':
+    start_find2()
+
