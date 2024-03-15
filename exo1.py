9557bebdc1de4bdc11e29a29fcc3d3c848fd56f1
Khaledsabane <148535782+Khaledsabane@users.noreply.github.com>
2024-03-15 19:54:37 +0100

    Update exo1.py

--- exo1.py
+++ exo1.py
@@ -1,7 +1,8 @@
 import sys
 if __name__=='__main__':
-    if len(sys.argv)==1:
-        print(f"Pas assez d’arguments pour le script nom duscript")
-    elif len(sys.argv) > 1:
+    if len(sys.argv)>1:
         for i in range(len(sys.argv)):
-            print(sys.argv[i])
+            print(f"         =>{sys.argv[i]}")
+
+    else:
+        print(f"Pas assez d'arguments")
